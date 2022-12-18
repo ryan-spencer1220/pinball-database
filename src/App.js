8877e7374095bd7e5f0df19a1cc5df1cb27a6e7d
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-12-17 19:42:17 -0800

    create homepage and initial api call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [machines, setMachines] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    console.log("dogs");
+
+    const fetchData = async () => {
+      const result = await axios("https://pinballmap.com/api/v1/machines.json");
+      setMachines(result.data.machines);
+      console.log(machines);
+    };
+    fetchData();
+    setLoading(false);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <body className="container grid-container">
+      <section className="">
+        <h1 className="fs-800 title">Pinball Database</h1>
+        <p className="ff-dm-sans intro">
+          Select your favorite pinball machines and learn about the history of
+          pinball! This application was created using PinballMap API.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </section>
+      <section className="search flex">
+        <form className="flex">
+          <select>
+            {machines?.map((machine) => (
+              <option>{machine.name}</option>
+            ))}
+          </select>
+          <input type="submit" value="search" className="button-74" />
+        </form>
+      </section>
+    </body>
   );
 }
 
